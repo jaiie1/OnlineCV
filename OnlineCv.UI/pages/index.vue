6f8d41d7943ed706dacbf09b00dfbaace3fea122
Mattias Jarl <jarl.mattias@gmail.com>
2020-08-10 15:18:05 +0200

    Get from rest api is now woring.

--- OnlineCv.UI/pages/index.vue
+++ OnlineCv.UI/pages/index.vue
@@ -1,39 +1,39 @@
 <template>
-  <div id="exampel">
-    <ul id="example-1">
-  <li v-for="item in items" :key="item.Jobbtitle">
-    {{ item.Jobbtitle }}
-  </li>
-    </ul>
+ <div class="Exampel">
 
+   <div>
+    <h1>Jobbs</h1>    
+   
+    <ul>
+      <li v-for="post of posts" :key="post.id">
+        {{ post.jobbTitel }}
+        {{ post.jobbDescription }}
+      </li>
+    </ul>
   </div>
+   
+ </div>
  
          
   
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
 import Axios from 'axios'
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 export default {
 
   name: "Exampel",
-
   data: () => ({
-    items:[
-      {Jobbtitle: "asd"}      
-    ]
+    posts: []     
   }),
-
-  asyncData(payload){
-    console.log("working");
-    return Axios.get("https://localhost:44357/api/jobbcontroller")
-    .then(({items}) => {
-      return {Jobbtitle: items}
-    })
-  }   
-  
+   asyncData (context) {
+      console.log("working");
+      return Axios.get("https://localhost:44357/api/jobbcontroller")
+       .then(({data}) => {
+      return {posts: data}
+    })    
+    },
+    fetchOnServer: false     
 }
 </script>
